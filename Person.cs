05db9117cbe00bd0fbb91ce7d4a05e7b4c9a2e1c
georgievg11 <114391110+georgievg11@users.noreply.github.com>
2023-05-18 10:23:49 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _26._09._2022
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public String Name
+        {  //реализираме свойство Name
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        { //реализираме свойство Age
+            get { return age; }
+            set { age = value; }
+        }
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("Здравейте! Аз съм {0} и съм на {1} години.", name, age);
+        }
+           
+    }
+}
